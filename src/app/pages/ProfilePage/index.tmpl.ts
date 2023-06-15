57b0c080a823219d92c7a676e5024f7c5eea12bd
Markeev Andrew <mccorn13@gmail.com>
2023-06-15 12:52:22 +0300

    События ProfilePage

--- src/app/pages/ProfilePage/index.tmpl.ts
+++ src/app/pages/ProfilePage/index.tmpl.ts
@@ -1,18 +1,3 @@
-import Button from "../../components/Button";
-import InputWithLabel from "../../components/InputWithLabel";
-
-type ProfilePropsData = {
-	data: {
-		display_name: string,
-		avatar: string,
-		login: string,
-		first_name: string,
-		second_name: string,
-		email: string,
-		phone: string,
-	}
-}
-
 const template = `
 <div class='page'>
 	<div class="wrapper wrapper_theme_default shadowed" >
@@ -24,72 +9,33 @@ const template = `
 						<h5>{data.display_name}</h5>
 					</div>
 					<div class="col4">
-					
+						{{{ inputAvatar }}}
 					</div>
 				</div>
 
 				<div class="column withGap article" >
-			
+					{{{ inputLogin }}}
+					{{{ inputDisplayName }}}
+					{{{ inputFirstName }}}
+					{{{ inputSecondName }}}
+					{{{ inputEmail }}}
+					{{{ inputPhone }}}
 				</div>
 
 				<div class="column withGap article" >
 					<h2>Password</h2>
 
-			
+					{{{ inputNewPassword }}}
+					{{{ inputOldPassword }}}
 				</div>
 
 				<div class="column column_align_center">
-				
+					{{{ button }}}
 				</div>
 			</form>
 		</main>
 	</div>
 </div>
 `
-
-const __template = (props: ProfilePropsData): string => {
-	const {data} = props;
-	return (
-		`
-		<div class='page'>
-			<div class="wrapper wrapper_theme_default shadowed" >
-				<main class="box" >
-					<form>
-						<div class="row article">
-							<div class="col8">
-								<h2>Profile Page</h2>
-								<h5>${data.display_name}</h5>
-							</div>
-							<div class="col4">
-								${(new InputWithLabel({ value: data.avatar, placeholder: "avatar", label: "avatar" })).render()}
-							</div>
-						</div>
-
-						<div class="column withGap article" >
-							${(new InputWithLabel({ value: data.login, placeholder: "login", label: "login" })).render()}
-							${(new InputWithLabel({ value: data.display_name, placeholder: "display_name", label: "display_name" })).render()}
-							${(new InputWithLabel({ value: data.first_name, placeholder: "first_name", label: "first_name" })).render()}
-							${(new InputWithLabel({ value: data.second_name, placeholder: "second_name", label: "second_name" })).render()}
-							${(new InputWithLabel({ value: data.email, placeholder: "email", label: "email" })).render()}
-							${(new InputWithLabel({ value: data.phone, placeholder: "phone", label: "phone" })).render()}
-						</div>
-
-						<div class="column withGap article" >
-							<h2>Password</h2>
-
-							${(new InputWithLabel({ value: "", placeholder: "newPassword", label: "newPassword" })).render()}
-							${(new InputWithLabel({ value: "", placeholder: "oldPassword", label: "oldPassword" })).render()}
-						</div>
-
-						<div class="column column_align_center">
-							${(new Button({ label: "Save" })).render()}
-						</div>
-					</form>
-				</main>
-			</div>
-		</div>
-		`
-	)
-}
 
 export default template;
--- src/app/pages/ProfilePage/index.ts
+++ src/app/pages/ProfilePage/index.ts
@@ -2,9 +2,58 @@ import tmpl from "./index.tmpl";
 import "./styles.less";
 
 import Block from "../../../utils/Block.js";
+import InputLazy from "../../components/InputLazy";
+import Button from "../../components/Button";
 
 export default class ProfilePage extends Block {
+	componentDidMount() {
+		this.state = {};
+	}
+
 	render() {
+		const handleFocusOut = (event) => {
+			this.state[event.target.name] = event.target.value
+		}
+		const handleSubmit = (event) => {
+			event.preventDefault();
+			console.log(this.state);
+		}
+
+		const inputEvents = { focusout: handleFocusOut };
+		const buttonEvents = {
+			click: (event) => handleSubmit(event, self),
+		}
+
+		const inputAvatar = new InputLazy('div', { value: "", name: "avatar", inputEvents: inputEvents });
+		
+		const inputLogin = new InputLazy('div', { value: "", name: "login", inputEvents: inputEvents });
+		const inputDisplayName = new InputLazy('div', { value: "", name: "display_name", inputEvents: inputEvents });
+		const inputFirstName = new InputLazy('div', { value: "", name: "first_name", inputEvents: inputEvents });
+		const inputSecondName = new InputLazy('div', { value: "", name: "second_name", inputEvents: inputEvents });
+		const inputEmail = new InputLazy('div', { value: "", name: "email", inputEvents: inputEvents });
+		const inputPhone = new InputLazy('div', { value: "", name: "phone", inputEvents: inputEvents });
+
+		const inputNewPassword = new InputLazy('div', { value: "", name: "newPassword", inputEvents: inputEvents });
+		const inputOldPassword = new InputLazy('div', { value: "", name: "oldPassword", inputEvents: inputEvents });
+
+		const button = new Button('div', { label: "Save", events: buttonEvents });
+
+		this.children = {
+			inputAvatar,
+
+			inputLogin,
+			inputDisplayName,
+			inputFirstName,
+			inputSecondName,
+			inputEmail,
+			inputPhone,
+
+			inputNewPassword,
+			inputOldPassword,
+			
+			button,
+		}
+
 		return this.compile(tmpl, this.props);
 	}
 }
