523602ac68ddb97731a21d7f560bc0c4d98a7e4e
Markeev Andrew <mccorn13@gmail.com>
2023-07-17 14:26:27 +0300

    Чистка

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,7 @@
 import App from "./blocks/App/index.js";
 import registerUIPartials, { UI } from "./ui.js";
 
-import AuthAPI from "./api/AuthAPI.js";
-import { USERS_DATA } from "./data.js";
-import { utils } from "../utils/index.js";
 import Store from "../utils/Store/index.js";
-
-// const INIT_DATA = utils.GEN.getInitData();
 
 registerUIPartials(UI);
 
