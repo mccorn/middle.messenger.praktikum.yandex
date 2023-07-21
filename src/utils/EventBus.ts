e52e96f5b18965568899603711b858cababe087b
Markeev Andrew <mccorn13@gmail.com>
2023-07-21 12:36:10 +0300

    замена any type

--- src/utils/EventBus.ts
+++ src/utils/EventBus.ts
@@ -1,20 +1,20 @@
 export interface IEventBus {
 	listeners: { [key: string]: FunctionStringCallback[] };
-	on: (eventName: string, callback: (...args: any) => any) => void;
-	off: (eventName: string, callback: (...args: any) => any) => void;
+	on: (eventName: string, callback: (...args: any) => unknown) => void;
+	off: (eventName: string, callback: (...args: any) => unknown) => void;
 	emit: (eventName: string, ...args: any) => void;
 }
 
 export default class EventBus implements IEventBus {
 	listeners: { [key: string]: FunctionStringCallback[] } = {};
 
-	on(eventName: string, callback: (...args: any) => any): void {
+	on(eventName: string, callback: (...args: any) => unknown): void {
 		if (!this.listeners[eventName]) this.listeners[eventName] = [];
 
 		this.listeners[eventName].push(callback)
 	}
 
-	off(eventName: string, callback: (...args: any) => any): void {
+	off(eventName: string, callback: (...args: any) => unknown): void {
 		if (!this.listeners[eventName]) return;
 
 		this.listeners[eventName] = this.listeners[eventName].filter(node => node !== callback)
